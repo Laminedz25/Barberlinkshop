03d454f997b0b7bdfb2ec393db8288c391009fa5
laminedz25 <lamine.sanfour25didou@gmail.com>
2026-03-16 01:28:58 +0100

    chore: optimize configs and replace firebase service account for VPS deployment

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1000,7 +1000,7 @@ export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ chil
   );
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
+ 
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
   if (!context) {
