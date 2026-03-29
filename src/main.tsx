f049a77a758f5aa87384f48d9791b1023a56efd6
laminedz25 <lamine.sanfour25didou@gmail.com>
2026-03-30 00:37:38 +0100

    System Upgrade: Deployed Service Worker to activate native Mobile Application installation (PWA)

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </ThemeProvider>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(
+      (registration) => console.log('Service Worker registered with scope:', registration.scope),
+      (err) => console.log('Service Worker registration failed:', err)
+    );
+  });
+}
+
